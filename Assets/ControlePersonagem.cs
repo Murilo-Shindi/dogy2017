b40a3f5f53d5899d0f53f273816d4af88fdf0ba0
Murilo-Shindi <murilo-shindi@hotmail.com>
2017-07-21 16:20:24 -0300

    Segundo Commit
    batata, amizade, europa, faustão

---
+++ Assets/ControlePersonagem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlePersonagem : MonoBehaviour {
+	Rigidbody2D rb;
+	[SerializeField] float velocidade;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent <Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.position += new Vector2 (Input.GetAxis ("Horizontal") * Time.deltaTime * velocidade, 0f);
+		transform.rotation = Quaternion.identity;
+	}
+}
